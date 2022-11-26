207525ede232a8aa3fcb7e45c6d7a91f82ade3d0
JessyHc <jessy.hoche@univ-rouen.fr>
2022-11-26 18:26:30 +0100

    feat(#BLOOGS-2): register confirmation

--- WebApplication1/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
+++ WebApplication1/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -2,15 +2,17 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
 using System.Text;
-using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MailKit.Security;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
+using MimeKit;
+using MimeKit.Text;
 using WebApplication1.Entities;
 
 namespace WebApplication1.Areas.Identity.Pages.Account
@@ -61,18 +63,31 @@ namespace WebApplication1.Areas.Identity.Pages.Account
 
             Email = email;
             // Once you add a real email sender, you should remove this code that lets you confirm the account
-            DisplayConfirmAccountLink = true;
-            if (DisplayConfirmAccountLink)
-            {
-                var userId = await _userManager.GetUserIdAsync(user);
-                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                EmailConfirmationUrl = Url.Page(
-                    "/Account/ConfirmEmail",
-                    pageHandler: null,
-                    values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
-                    protocol: Request.Scheme);
-            }
+          
+            var emailMessage = new MimeMessage();
+            //Confirmation link generation
+            var userId = await _userManager.GetUserIdAsync(user);
+            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+            EmailConfirmationUrl = Url.Page(
+                "/Account/ConfirmEmail",
+                pageHandler: null,
+                values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
+                protocol: Request.Scheme);
+
+            //Setup email message
+            emailMessage.From.Add(MailboxAddress.Parse("from_address@example.com"));
+            emailMessage.To.Add(MailboxAddress.Parse(email));
+            emailMessage.Subject = "Registration Confirmation";
+            emailMessage.Body = new TextPart(TextFormat.Html) { Text =
+                "Please confirm your account by clicking <a href=\"" + EmailConfirmationUrl + "\">here</a>"
+            };
+
+            // send email
+            using var smtp = new SmtpClient();
+            smtp.Connect("localhost", 1025, SecureSocketOptions.Auto);
+            smtp.Send(emailMessage);
+            smtp.Disconnect(true);
 
             return Page();
         }
