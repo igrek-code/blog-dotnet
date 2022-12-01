20ebcd67726a03b9318d14831b958232bbb6fcea
lydia <lydiakahouadji06@gmail.com>
2022-12-01 10:29:16 +0100

    Gestion des roles : Ajout d'un role à la création

--- WebApplication1/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ WebApplication1/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -130,9 +130,12 @@ namespace WebApplication1.Areas.Identity.Pages.Account
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
+                
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+                    await _userManager.AddToRoleAsync(user, "USER"); 
+
 
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
