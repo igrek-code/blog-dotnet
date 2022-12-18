356a252574d67acb431b4b07f8006e2cc73cbac8
JessyHc <jessy.hoche@univ-rouen.fr>
2022-12-18 15:46:55 +0100

    test fix

---
+++ Bloogs/Models/Comment.cs
@@ -0,0 +1,10 @@
+﻿using WebApplication1.Entities;
+
+namespace Bloogs.Models;
+
+public class Comment
+{
+    public int Id { get; set; }
+    public string Content { get; set; }
+    public User Owner { get; set; }
+}
---
+++ Bloogs/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebApplication1.Entities;
+
+namespace Bloogs.Models;
+
+public class Post
+{
+    public int Id { get; set; }
+    [DataType(DataType.MultilineText)]
+    public string Content { get; set; }
+    public string ImagesUrl { get; set; }
+    public User Poster { get; set; }
+    public List<Comment>? Comments { get; set; }
+    public List<User>? Likers { get; set; }
+
+}
