0f5b596e76744e38acb325f86641861961c86128
Yacine <bouyakoubyacine1@gmail.com>
2023-01-06 16:05:48 +0100

    fix: supervisor seed account

---
+++ Bloogs/Data/UserService.cs
@@ -0,0 +1,40 @@
+﻿using Bloogs.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace Bloogs.Data
+{
+    public class UserService: IUserService
+    {
+        private readonly SignInManager<User> _signInManager;
+        public UserService(SignInManager<User> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<User> SuperVisorAccountSeed()
+        {
+            var password = "admin";
+
+            // Seed Users
+            var supervisorUser = new User
+            {
+                //Email = "supervisor@blog.fr",
+                EmailConfirmed = true,
+                FirstName = "Supervisor",
+                LastName = ""
+            };
+            await _signInManager.UserManager.SetUserNameAsync(supervisorUser, "supervisor@blog.fr");
+            await _signInManager.UserManager.SetEmailAsync(supervisorUser, "supervisor@blog.fr");
+
+            var result = await _signInManager.UserManager.CreateAsync(supervisorUser, password);
+
+            await _signInManager.UserManager.AddToRoleAsync(supervisorUser, "Supervisor");
+
+            var token = await _signInManager.UserManager.GenerateEmailConfirmationTokenAsync(supervisorUser);
+
+            await _signInManager.UserManager.ConfirmEmailAsync(supervisorUser, token);
+
+            return supervisorUser;
+        }
+    }
+}
--- Bloogs/Program.cs
+++ Bloogs/Program.cs
@@ -40,6 +40,7 @@ builder.Services.Configure<RequestLocalizationOptions>(options =>
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSyncfusionBlazor();
+builder.Services.AddScoped<IUserService, UserService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -66,6 +67,16 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.MapRazorPages();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<AppDbContext>();
+    var userService = services.GetRequiredService<IUserService>();
+    context.Database.Migrate();
+    await userService.SuperVisorAccountSeed();
+}
 
 app.MapControllerRoute(
     name: "Areas",
